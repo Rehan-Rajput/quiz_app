55065d30931332cc390a99389b3e89a23328eb0c
Rehan-Rajput <77195218+Rehan-Rajput@users.noreply.github.com>
2021-01-27 11:40:24 -0500

    updated

--- src/Components/QuestionCard.tsx
+++ src/Components/QuestionCard.tsx
@@ -1,43 +1,43 @@
 import React from 'react';
-import {AnswerObject} from '../App';
+import { AnswerObject } from '../App';
 import { Wrapper, ButtonWrapper } from './QuestionCard.styles';
 
 type Props = {
     question: string;
     answers: string[];
-    callback:  (e: React.MouseEvent<HTMLButtonElement>) => void;
+    callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
     userAnswer: AnswerObject | undefined;
     questionNo: number;
     totalQuestions: number;
 }
 
 
-const QuestionCard: React.FC<Props> = ({ 
+const QuestionCard: React.FC<Props> = ({
     question,
-    answers, 
-    callback, 
-    userAnswer, 
-    questionNo, 
-    totalQuestions 
+    answers,
+    callback,
+    userAnswer,
+    questionNo,
+    totalQuestions
 }) => {
     return (
         <Wrapper>
             <p className="number">
                 Question: {questionNo} / {totalQuestions}
             </p>
-            <p dangerouslySetInnerHTML = {{ __html:question}} />
+            <p dangerouslySetInnerHTML={{ __html: question }} />
             <div>
-               {answers.map(answer => (
-                   <ButtonWrapper  
-                    key={answer}
-                    correct = {userAnswer?.correctAnswer === answer}
-                    userClicked = {userAnswer?.answer === answer}
-                   >
-                       <button disabled={userAnswer ? true : false} value={answer} onClick={callback}>
-                         <span dangerouslySetInnerHTML={{ __html: answer }} />
-                       </button>
-                   </ButtonWrapper>
-               ))}
+                {answers.map(answer => (
+                    <ButtonWrapper
+                        key={answer}
+                        correct={userAnswer?.correctAnswer === answer}
+                        userClicked={userAnswer?.answer === answer}
+                    >
+                        <button disabled={userAnswer ? true : false} value={answer} onClick={callback}>
+                            <span dangerouslySetInnerHTML={{ __html: answer }} />
+                        </button>
+                    </ButtonWrapper>
+                ))}
             </div>
         </Wrapper>
     )
